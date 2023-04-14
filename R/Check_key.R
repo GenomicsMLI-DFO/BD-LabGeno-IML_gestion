8c774aa4dd9ac75b6daf61f1e53456cbf7f57632
Bourret <audrey.bourret@usherbrooke.ca>
2023-04-14 14:47:20 -0400

    Add a function to check KEY

---
+++ R/Check_key.R
@@ -0,0 +1,124 @@
+#' @title Check that we are not trying to import data that should be already present
+#'
+#' @description
+#' Function to check that column name and order follow the predefined template
+#'
+#' @details
+#' NULL if you doesn't want a specif sheet to be uploaded. No other check than sheet name.
+#'
+#' @param data List containing important tables.
+#'
+#' @examples
+#' # provide some examples of how to use your function
+#'
+#'
+#' @seealso [upload_gabarit_ADN()] to create the list of tables.
+#'
+#` @references
+#' List references
+#' @export
+
+check_key <- function(data, DB = "LabGeno"){
+
+  cat(crayon::green("\nLooking that new data are not already within the DB!\n"))
+
+  # Establish the connection
+  con <- RODBC::odbcConnect(DB)
+  # Stop if you cannot connect
+  if(is.null(attr(con, "connection.string"))){
+    stop("Something went wrong with the connection ...")
+    # Perform the function if you can connect
+  }
+
+  res <- sapply(stringr::str_split(attr(con, "connection.string"), ";"), `[`,2) |> stringr::str_remove("DBQ=")
+  cat(paste("\nConnected to", res, "\n"))
+
+  # GROUPE
+  if(c("Groupes") %in%  names(data)  ){
+
+    cat("\nImporting", crayon::cyan("Groupes"), "KEY:\n")
+
+    Numero_unique_groupe   <- data$Groupes$Numero_unique_groupe
+
+    groupes.key <- load_columns_DB(columns = "Numero_unique_groupe", table = "01_Groupes", DB = DB, verbose = F) |> dplyr::pull(Numero_unique_groupe)
+
+    commun <- NULL
+    commun <- base::intersect(Numero_unique_groupe, groupes.key)
+
+    cat("\n", length(Numero_unique_groupe), "new Numero_unique_groupe detected\n")
+
+    if(length(commun) > 0){
+      cat(crayon::red("\n", length(commun), "keys are already present in the DB:", paste(commun, collapse = ", "),
+                      "\nThis will be problematic with the importation into ACCESS...\n"))
+    } else{
+    cat(crayon::green("\nNo new Numero_unique_groupe already present in the DB, it's perfect!\n"))
+    }
+    }
+
+  # SPECIMEN
+
+  if(c("Specimens") %in%  names(data)  ){
+    cat("\nImporting", crayon::cyan("Specimens"), "KEY:\n")
+
+    Numero_unique_specimen   <- data$Specimens$Numero_unique_specimen
+
+    specimen.key <- load_columns_DB(columns = "Numero_unique_specimen", table = "02_Specimens", DB = DB, verbose = F) |> dplyr::pull(Numero_unique_specimen)
+
+    commun <- NULL
+    commun <- base::intersect(Numero_unique_specimen, specimen.key)
+
+    cat("\n", length(Numero_unique_specimen), "new Numero_unique_specimen detected\n")
+
+    if(length(commun) > 0){
+      cat(crayon::red("\n", length(commun), "keys are already present in the DB:", paste(commun, collapse = ", "),
+                      "\nThis will be problematic with the importation into ACCESS...\n"))
+    }else{
+      cat(crayon::green("\nNo new Numero_unique_specimen already present in the DB, it's perfect!\n"))
+  }
+}
+
+  # TISSUS
+
+  if(c("Tissus") %in%  names(data)  ){
+    cat("\nImporting", crayon::cyan("Tissus"), "KEY:\n")
+
+    Numero_unique_tissu  <- data$Tissus$Numero_unique_tissu
+
+    tissu.key <- load_columns_DB(columns = "Numero_unique_tissu", table = "03_Tissus", DB = DB, verbose = F) |> dplyr::pull(Numero_unique_tissu)
+
+    commun <- NULL
+    commun <- base::intersect(Numero_unique_tissu, tissu.key)
+
+    cat("\n", length(Numero_unique_tissu), "new Numero_unique_tissu detected\n")
+
+    if(length(commun) > 0){
+      cat(crayon::red("\n", length(commun), "keys are already present in the DB:", paste(commun, collapse = ", "),
+                      "\nThis will be problematic with the importation into ACCESS...\n"))
+    } else{
+      cat(crayon::green("\nNo new Numero_unique_tissu already present in the DB, it's perfect!\n"))
+    }
+    }
+
+  # EXTRAIT
+
+  if(c("Extraits_ADN_ARN") %in%  names(data)  ){
+    cat("\nImporting", crayon::cyan("Extraits_ADN_ARN"), "KEY:\n")
+
+    Numero_unique_extrait   <- data$Extraits_ADN_ARN$Numero_unique_extrait
+
+    extrait.key <- load_columns_DB(columns = "Numero_unique_extrait", table = "04_Extraits_ADN_ARN", DB = DB, verbose = F) |> dplyr::pull(Numero_unique_extrait)
+
+    commun <- NULL
+    commun <- base::intersect(Numero_unique_extrait, extrait.key)
+
+    cat("\n", length(Numero_unique_extrait), "new Numero_unique_extrait detected\n")
+
+    if(length(commun) > 0){
+      cat(crayon::red("\n", length(commun), "keys are already present in the DB:", paste(commun, collapse = ", "),
+                      "\nThis will be problematic with the importation into ACCESS...\n"))
+    }  else{
+      cat(crayon::green("\nNo new Numero_unique_extrait already present in the DB, it's perfect!\n"))
+  }}
+
+
+} # END of the function
--- R/Connect_DB.R
+++ R/Connect_DB.R
@@ -69,7 +69,6 @@ load_DB <- function(table = "00_Liste_projets", DB = "LabGeno"){
 
     cat(paste("\nThe table", table, "was uploaded and contained", nrow(export.df), "entries.\n"))
 
-
   }
 
   RODBC::odbcCloseAll()
@@ -77,5 +76,62 @@ load_DB <- function(table = "00_Liste_projets", DB = "LabGeno"){
   return(export.df)
 
 } # End of function
+
+#' @title Upload specific column of a DB table
+#'
+#' @description
+#' Function to upload one or more specific columns from a the table in database
+#'
+#' @details
+#' Possible tables are :  00_Liste_projets, 01_Groupes, 02_Specimens, 03_Tissus, 04_Extraits_ADN_ARN, 05_Analyse_Externe, 06_Sexage, 07_Sequencage, 08_Hormones, 09_GQ
+#'
+#' @param columns Vector of the columne to upload.
+#' @param table Name of the table you want to load.
+#' @param DB Name of the OBDC database, as set in your computer.
+#' @param verbose Print info if TRUE.
+#'
+#' @examples
+#' # Not to run
+#' res <-load_columns_DB(columns = c("Numero_unique_groupe"), table = "01_Groupes")
+#' head(res)
+#' @seealso
+#'
+#` @references
+#' List references
+#' @export
+
+
+
+load_columns_DB <- function(columns, table, DB = "LabGeno", verbose = TRUE){
+
+  if (missing(columns)| missing(table)) {
+    stop("Please provide a value for both columns and table argument.")
+  }
+
+  # Establish the connection
+  con <- RODBC::odbcConnect(DB)
+  # Stop if you cannot connect
+  if(is.null(attr(con, "connection.string"))){
+    print("Something went wrong with the connection ...")
+  # Perform the function if you can connect
+  } else {
+
+    res <- sapply(stringr::str_split(attr(con, "connection.string"), ";"), `[`,2) |> stringr::str_remove("DBQ=")
+
+    if(verbose == T) cat(paste("\nConnected to", res))
+
+    qry.1 <- paste("SELECT", paste(columns, sep = ", "), "FROM", paste0("[",table,"]"))
+
+    export.df <- RODBC::sqlQuery(con, qry.1)
+
+    if(verbose == T) cat(paste("\nThe columns from the table", table, "were uploaded and contained", nrow(export.df), "entries.\n"))
+
+  }
+
+  RODBC::odbcCloseAll()
+
+  return(export.df)
+
+}
 
 
