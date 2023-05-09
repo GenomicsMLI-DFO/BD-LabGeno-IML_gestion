db27e050c87ed387ba00b7156f583e52ba696a3d
Bourret <audrey.bourret@usherbrooke.ca>
2023-05-09 15:58:24 -0400

    Add upload eDNA gabarrit

--- R/Upload_gabarit.R
+++ R/Upload_gabarit.R
@@ -1,4 +1,4 @@
-#' @title Upload Excel template
+#' @title Upload Excel DNA template
 #'
 #' @description
 #' Function to upload an Excel file based on the predefined template
@@ -49,7 +49,8 @@ upload_gabarit_ADN <- function(path,
   sheet.to.load <- c(specimen, groupe, tissu, extraitADN, analyse_ext, sexage, sequencage, hormone)
 
   if( !all(sheet.to.load %in% sheet.observed)){
-         stop(paste("\nThe sheet to be uploaded (",  paste(sheet.to.load, collapse = ", "),") doesn't fit the one detected. You can modified their names as an argument within this function or directly within the Excel file. NULL can be added as an argument if a sheet is totally absent."))
+    stop(paste("\nThe sheet to be uploaded doesn't fit the one detected.\n\nThese ones are problematics:", paste(setdiff(sheet.to.load, sheet.observed), collapse = ", "),
+               "\n\nYou can modified their names as an argument within this function or directly within the Excel file. NULL can be added as an argument if a sheet is totally absent."))
   }
 
   # Etape 2 - loader chacune des pages, et les mettre dans une liste
@@ -338,6 +339,376 @@ cat(crayon::green("\n",n.test, "tables have been combined", emojifont::emoji("bo
 
 return(combine.ls)
 
+
+}
+
+
+#' @title Upload Excel eDNA template
+#'
+#' @description
+#' Function to upload an Excel file based on the predefined eDNA template
+#'
+#' @details
+#' NULL if you doesn't want a specif sheet to be uploaded. No other check than sheet name.
+#'
+#' @param path Path to the excel spreadsheet
+#' @param skip N row to skip. Default value is 0.
+#' @param site Name of the sheet in the Excel file containing site data
+#' @param station Name of the sheet in the Excel file containing station data
+#' @param echantillon Name of the sheet in the Excel file containing sample data
+#' @param filtre Name of the sheet in the Excel file containing the filter data
+#' @param extrait Name of the sheet in the Excel file containing extract data
+#' @param qPCR_inhibition of the sheet in the Excel file containing the qpcr inihibiton info
+#' @param qPCR Name of the sheet in the Excel file containing the qpcr results
+#' @param sequencage Name of the sheet in the Excel file containing the Sanger sequencing info
+#' @param courbe Name of the sheet in the Excel file containing the standard curve info
+#' @param librairie Name of the sheet in the Excel file containing the librairy info
+#' @param purification Name of the sheet in the Excel file containing the librairy purification info
+#' @param analyses_ext_lib Name of the sheet in the Excel file containing the librairy sequencing info
+#'
+#' @examples
+#' # provide some examples of how to use your function
+#'
+#'
+#' @seealso
+#'
+#` @references
+#' List references
+#' @export
+upload_gabarit_ADNe <- function(path,
+                                skip=0,
+                                site = "Sites_ADNe",
+                                station = "Stations_ADNe",
+                                echantillon = "Echantillons_ADNe",
+                                filtre = "Filtres_ADNe",
+                                extrait = "Extraits_ADNe",
+                                qpcr_inhibition = "qPCR_inhibition_ADNe",
+                                qpcr = "qPCR_ADNe",
+                                sequencage = "Sequencage_Sanger_ADNe",
+                                courbe = "Courbe_etalonnage_ADNe",
+                                librairies = "Librairie_ADNe",
+                                purification = "Purification_librairies_ADNe",
+                                analyses_ext_lib = "Analyses_externes_librairies_ADNe"
+){
+  # Etape 1 - verifier que les noms suivent le gabarit
+
+  `%nin%` = Negate(`%in%`)
+
+  sheet.observed <- readxl::excel_sheets(path = path)
+
+  cat("\nThe sheets detected are",  paste(sheet.observed, collapse = ", "), "\n")
+
+  sheet.to.load <- c(site, station, echantillon, filtre, extrait, qpcr_inhibition, qpcr, sequencage, courbe, librairies, purification, analyses_ext_lib)
+
+  if( !all(sheet.to.load %in% sheet.observed)){
+    stop(paste("\nThe sheet to be uploaded doesn't fit the one detected.\n\nThese ones are problematics:", paste(setdiff(sheet.to.load, sheet.observed), collapse = ", "),
+               "\n\nYou can modified their names as an argument within this function or directly within the Excel file. NULL can be added as an argument if a sheet is totally absent."))
+  }
+
+  # Etape 2 - loader chacune des pages, et les mettre dans une liste
+  # On pourrait imprimer certaines statistiques
+
+  excel.ls <- list()
+
+  # Load sites
+
+  if(!is.null(site)){
+
+    cat("\nLoading", crayon::cyan("Sites_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = site, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Sites_ADNe"]] <- temp.df
+
+    }
+
+  }
+
+  # Load station
+
+  if(!is.null(station)){
+
+    cat("\nLoading", crayon::cyan("Stations_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = station, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Stations_ADNe"]] <- temp.df
+
+    }
+
+  }
+
+
+  # Load echantillon
+
+  if(!is.null(echantillon)){
+
+    cat("\nLoading", crayon::cyan("Echantillons_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = echantillon, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Echantillons_ADNe"]] <- temp.df
+
+    }
+
+  }
+
+  # Load filtre
+
+  if(!is.null(filtre)){
+
+    cat("\nLoading", crayon::cyan("Filtres_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = filtre, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Filtres_ADNe"]] <- temp.df
+
+    }
+
+  }
+
+
+
+  # Load extrait
+
+  if(!is.null(extrait)){
+
+    cat("\nLoading", crayon::cyan("Extraits_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = filtre, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Extrait_ADNe"]] <- temp.df
+
+    }
+
+    }
+    # Load qpcr_inhibition
+
+    if(!is.null(qpcr_inhibition)){
+
+      cat("\nLoading", crayon::cyan("qPCR_inhibition_ADNe"),"\n")
+
+      temp.df <-  readxl::read_excel(path = path, sheet = qpcr_inhibition, skip = skip,col_types = "text", .name_repair = "minimal")
+
+      cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+      if(nrow(temp.df)>0){
+        dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+        if(length(dup.prob > 0)){
+
+          cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+        }
+
+        excel.ls[["qPCR_inhibition_ADNe"]] <- temp.df
+
+      }
+
+      }
+
+      # Load qpcr
+
+      if(!is.null(qpcr)){
+
+        cat("\nLoading", crayon::cyan("qPCR_ADNe"),"\n")
+
+        temp.df <-  readxl::read_excel(path = path, sheet = qpcr, skip = skip,col_types = "text", .name_repair = "minimal")
+
+        cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+        if(nrow(temp.df)>0){
+          dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+          if(length(dup.prob > 0)){
+
+            cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+          }
+
+          excel.ls[["qPCR_ADNe"]] <- temp.df
+
+        }
+
+        }
+
+
+  # Load sequencage
+
+  if(!is.null(sequencage)){
+
+    cat("\nLoading", crayon::cyan("Sequencage_Sanger_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = sequencage, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Sequencage_Sanger_ADNe"]] <- temp.df
+
+    }
+
+  }
+
+
+  # Load courbe
+
+  if(!is.null(courbe)){
+
+    cat("\nLoading", crayon::cyan("Courbe_etalonnage_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = courbe, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Courbe_etalonnage_ADNe"]] <- temp.df
+
+    }
+
+  }
+
+  # Load librairies
+
+  if(!is.null(librairies)){
+
+    cat("\nLoading", crayon::cyan("Librairie_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = librairies, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Librairie_ADNe"]] <- temp.df
+
+    }
+
+  }
+
+  # Load purification
+
+  if(!is.null(purification)){
+
+    cat("\nLoading", crayon::cyan("Purification_librairies_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = purification, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Purification_librairies_ADNe"]] <- temp.df
+
+    }
+
+  }
+
+  # Load Analyses_externes_librairies_ADNe
+
+  if(!is.null(analyses_ext_lib)){
+
+    cat("\nLoading", crayon::cyan("Analyses_externes_librairies_ADNe"),"\n")
+
+    temp.df <-  readxl::read_excel(path = path, sheet = analyses_ext_lib, skip = skip,col_types = "text", .name_repair = "minimal")
+
+    cat("A dataframe of", ncol(temp.df), "columns and", nrow(temp.df), "rows was uploaded\n")
+
+    if(nrow(temp.df)>0){
+      dup.prob <- names(temp.df)[duplicated(names(temp.df))]
+      if(length(dup.prob > 0)){
+
+        cat(crayon::red("WARNING: The column(s)", paste(dup.prob, collapse = ", "), "appeared more than one time, you should check this prior to the importation in R ...\n"))
+
+      }
+
+      excel.ls[["Analyses_externes_librairies_ADNe"]] <- temp.df
+
+    }
+
+  }
+
+
+
+  # Etape 3 - retourner la liste - c'est à partir d'elle qu'on va travailler
+
+  cat(crayon::green("\nThe sheets", paste(names(excel.ls), collapse = ", "), "have been uploaded", emojifont::emoji("unicorn")  ,"\n\n"))
+
+  return(excel.ls)
 
 }
 
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,6 +5,7 @@ model.ID   <-  readxl::read_excel("inst/BD_format.xlsx", sheet = "ID")
 model.list <-  readxl::read_excel("inst/BD_format.xlsx", sheet = "Valeurs")
 model.date <- readxl::read_excel("inst/BD_format.xlsx", sheet = "Date")
 model.other <- readxl::read_excel("inst/BD_format.xlsx", sheet = "Others")
+model.eDNA_convert <- readxl::read_excel("inst/BD_format.xlsx", sheet = "Modif_ADNe_column")
 
 # Save the cleaned data in the required R package location
 
